52e14aa9bac7415ace080455e910b7fc1dc51e94
Victor <victoe680@gmail.com>
2021-03-03 13:49:18 -0600

    cambios de estilo en blog

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -25,18 +25,18 @@ class Card extends Component<IProps, IState> {
                         <img src={img} className="img-responsive" alt="IMG12" />
                         <div className="post-overlay">
                             <span>
-                                <a href="#">14 <br /><small>Feb</small></a>
+                                <a href="#">11 <br /><small>Feb</small></a>
                             </span>
                         </div>
                     </div>
                     <div className="post-content">
-                        <h2 className="post-title"><Link to="#">Advanced business cards design</Link></h2>
-                        <h3 className="post-author"><a href="#">Posted by micron News</a></h3>
+                        <h2 className="post-title"><Link to="#">Título del artículo</Link></h2>
+                        <h3 className="post-author"><a href="#">Josías J. Martínez</a></h3>
                         <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber [...]</p>
-                        <a href="/blog" className="read-more">View More</a>
+                        <Link to="/blog" className="read-more">Leer más</Link>
                         <div className="post-bottom">
-                            <span className="post-date pull-left">February 11, 2014</span>
-                            <span className="comments-number pull-right"><a href="#">3 comments</a></span>
+                            <span className="post-date pull-left">11 febrero de 2014</span>
+                            <span className="comments-number pull-right"><a href="#">3 comentarios</a></span>
                         </div>
                     </div>
                 </div>
